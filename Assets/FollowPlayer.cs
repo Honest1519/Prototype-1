aa2d7213d3f754f28940c1642ca934a489112387
Payton <56128570+Honest1519@users.noreply.github.com>
2019-10-22 09:50:05 -0700

    Camera & Variables
    I created 2 public variables- 1 gameobject (player), and 1 value (speed). I also created 1 private varible, a vector3 value (offset).

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
-    public float speed = 20;
+    public GameObject player;
+    private Vector3 offset = new Vector3 (0, 5, -7);
     // Update is called once per frame
     void Update()
     {
-        // Moving the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+       transform.position = player.transform.position + offset;
     }
 }
